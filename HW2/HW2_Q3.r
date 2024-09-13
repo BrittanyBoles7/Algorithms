ffc5124b027cf50baa914a988da25bfafb4b8db9
Madie Munro <madiemunro00@gmail.com>
2024-09-13 11:34:34 -0600

    changed code

--- HW2/HW2_Q3.r
+++ HW2/HW2_Q3.r
@@ -18,10 +18,20 @@ A <- c(3, 5, 7, 9)
 B <- c(2, 4, 6, 8)
 n <- length(A)
 
-smallest_median_value(A, B, n)
+smallest_median_value(A, B, n) 
 
 A <- c(6, 8, 13, 15, 19)
 B <- c(3, 7, 10, 11, 16)
 n <- length(A)
 
 smallest_median_value(A, B, n)
+
+A <- c(1,4,8,12,20,25,50,65,67,79)
+B <- c(2,3,5,19,21,28,40,59,70,80)
+n <- length(A)
+
+smallest_median_value(A, B, n)
+
+
+
+
