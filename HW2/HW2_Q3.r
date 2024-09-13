1c0b409514f08fc1ab870765a60a017cae9dbbf5
Madie Munro <madiemunro00@gmail.com>
2024-09-13 13:01:48 -0600

    modified algorithm slightly

--- HW2/HW2_Q3.r
+++ HW2/HW2_Q3.r
@@ -1,36 +1,34 @@
 # calculate the nth smallest median between two arrays of the same size
 
-smallest_median_value <- function(A, B, n) {
-    if (n == 1) {
+smallest_median_value <- function(A, B) {
+  n <- length(A)  
+  if (n == 1) {
         return(min(A[n], B[n]))  #base case
     }
   
     k <- floor(n/2) #starting median
     
     if (A[k] < B[k]) {
-        return(smallest_median_value(A[(k+1):n], B, n - k))
+        return(smallest_median_value(A[(k+1):n], B[1:(n-k)]))
     } else {
-        return(smallest_median_value(A, B[(k+1):n], n - k))
+        return(smallest_median_value(A[1:(n-k)], B[(k+1):n]))
     }
 }
 
 A <- c(3, 5, 7, 9)
 B <- c(2, 4, 6, 8)
-n <- length(A)
 
-smallest_median_value(A, B, n) 
+smallest_median_value(A, B) 
 
 A <- c(6, 8, 13, 15, 19)
 B <- c(3, 7, 10, 11, 16)
-n <- length(A)
 
-smallest_median_value(A, B, n)
+smallest_median_value(A, B)
 
 A <- c(1,4,8,12,20,25,50,65,67,79)
 B <- c(2,3,5,19,21,28,40,59,70,80)
-n <- length(A)
 
-smallest_median_value(A, B, n)
+smallest_median_value(A, B)
 
 
 
